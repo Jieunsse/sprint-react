2421f2e4cf9995041aa0b1be614c6661d480d82e
jieunsse <jieunsse@gmail.com>
2026-01-23 01:18:17 +0900

    feat: 헤더 컴포넌트 추가

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,43 @@
-export default function Header() {
+import { Link, useLocation } from 'react-router-dom';
+import { headerContainer, navItem, profileImage } from './header.styles';
+import { cn } from '@shared/utils/cn';
+
+import LogoPc from '../header/svgs/logo/logo.svg';
+import LogoMobile from '../header/svgs/logo/logo-mobile.svg';
+import ProfileIcon from '../header/svgs/profile/profile.svg';
+
+export function Header() {
+  const { pathname } = useLocation();
+  const isFreePage = pathname === '/';
+  const isItemsPage = pathname === '/items';
+
   return (
-    <>
-      <header>Header</header>
-    </>
+    <header className={headerContainer()}>
+      <div className="flex items-center">
+        <Link to="/" className="flex items-center">
+          <img
+            src={LogoPc}
+            alt="logo"
+            className="hidden md:block h-[52px] w-[156px] object-contain cursor-pointer"
+          />
+          <img
+            src={LogoMobile}
+            alt="logo"
+            className="block md:hidden h-[40px] w-[84px] object-contain cursor-pointer"
+          />
+        </Link>
+
+        <nav className="ml-8 flex items-center gap-4">
+          <Link to="/" className={cn(navItem({ active: isFreePage }))}>
+            자유게시판
+          </Link>
+          <Link to="/items" className={cn(navItem({ active: isItemsPage }))}>
+            중고마켓
+          </Link>
+        </nav>
+      </div>
+
+      <img src={ProfileIcon} alt="profile" className={profileImage()} />
+    </header>
   );
 }
