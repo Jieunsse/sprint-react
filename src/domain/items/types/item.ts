b82f332bb1030842e3af70ce0b47d96a20001ebd
jieunsse <jieunsse@gmail.com>
2026-01-24 21:03:40 +0900

    feat: 쿼리 get 요청 추가

---
+++ src/domain/items/types/item.ts
@@ -0,0 +1,17 @@
+export interface Item {
+  id: number;
+  name: string;
+  description: string;
+  price: number;
+  images: string[];
+  tags: string[];
+  favoriteCount: number;
+  ownerId: number;
+  ownerNickname: string;
+  createdAt: string;
+}
+
+export interface GetItemsResponse {
+  totalCount: number;
+  list: Item[];
+}
