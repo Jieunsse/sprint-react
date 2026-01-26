917d389a9a9f368f2747c3b24fa4274951fc0131
jieunsse <jieunsse@gmail.com>
2026-01-26 14:35:25 +0900

    feat: addItem 페이지 라우팅 경로 추가

--- src/app/routes/Router.tsx
+++ src/app/routes/Router.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Home from '../../domain/home/Home';
 import ItemsPage from '@src/domain/items/pages/ItemsPage';
+import AddItemPage from '@src/domain/addItem/pages/AddItemPage';
 
 export const router = createBrowserRouter([
   {
@@ -10,5 +11,9 @@ export const router = createBrowserRouter([
   {
     path: '/items',
     element: <ItemsPage />,
+  },
+  {
+    path: '/additems',
+    element: <AddItemPage />,
   },
 ]);
